7a04e0132404736ca6ef701863436dd0990994d7
NandhaKumar1717 <nandhakumaran1717@gmail.com>
2025-09-02 22:41:58 +0530

    Fix production environment configuration for dashboard

--- dashboard/webpack.prod.config.js
+++ dashboard/webpack.prod.config.js
@@ -1,1 +1,8 @@
-module.exports = require('./webpack.config');
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+  remotes: {},
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+});
