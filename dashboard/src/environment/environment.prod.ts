51f7c12c5c7bebc1f20e3facdf60b32fd4d726c4
NandhaKumar1717 <NandhaKumar1717@users.noreply.github.com>
2025-09-02 16:19:39 +0000

    deploy: 23e0a3cd13547a42d51735e2910ee018f888b3a1

--- dashboard/src/environment/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production : true,
-    remotes: {
-        supplyChain: 'https://nandhakumar1717.github.io/frontcraft/supply-chain/remoteEntry.js'
-    }
-}
--- dashboard/src/environment/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  remotes: {
-    supplyChain: 'http://localhost:4201/remoteEntry.js',
-  }
-  };
